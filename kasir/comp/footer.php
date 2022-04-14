43e904b9c4b9fdefba1fe2107ad1798025d25e56
Zaidan-Fikri <zaidanfikri11@gmail.com>
2022-04-14 07:54:25 +0700

    kasir

--- kasir/comp/footer.php
+++ kasir/comp/footer.php
@@ -1,7 +1,7 @@
 
 <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> Zibran | Powered By <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; <script>document.write(new Date().getFullYear());</script> Powered By <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.5
